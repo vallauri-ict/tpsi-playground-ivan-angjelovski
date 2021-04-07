31769f93694b75487e3bc8b6d80e26cd30c32cff
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-04-07 12:48:52 +0200

    Added Es05_Percorso

---
+++ Geolocation/Es05_Percorso/README.md
@@ -0,0 +1,8 @@
+5. > **LEZIONE 5**
+     5. > *Consegna GeoLocation - Ese 5 Percorso*
+
+## Obiettivo:
+L'utente deve inserire il luogo di partenza e di arrivo in 2 textbox. In seguito visualizzerà i diversi percorsi attraverso i quali si può raggiungere la destinazione sulla mappa.
+
+## Al click del pulsante visualizza percorso:
+Tramite una new google.maps.DirectionsService() è possibile ottenere l'elenco dei percorsi migliori per il tragitto selezionato dall'utente. Tramite il DirectionsRenderer() viene visualizzata la google map ed il percorso migliore (modalità di viaggio impostata come driving).
---
+++ Geolocation/Es05_Percorso/index.js
@@ -0,0 +1,87 @@
+﻿"use strict"
+
+$(document).ready(function(){
+	let header =$("#header");
+	let button = header.children("input");
+	let partenza = header.find("input").eq(0);
+	let arrivo = header.find("input").eq(1);
+	let wrapper = $("#wrapper");
+	let map =  wrapper.children(".map")[0];     // js
+	let panel= wrapper.children(".panel")[0];   // js
+	let msg =  wrapper.children(".msg");
+
+
+	button.on("click", function(){
+		if (partenza.val() == "" || arrivo.val() == "") 
+			alert("Prego compilare i campi di partenza e arrivo")
+		else{
+			let geocoder = new google.maps.Geocoder();
+			geocoder.geocode({"address": partenza.val()}, function(resultPartenza, status){
+				if(status != google.maps.GeocoderStatus.OK){
+					alert("Stringa immessa non valida");
+				} else {
+					geocoder.geocode({"address": arrivo.val()}, function(resultArrivo, status){
+						if(status != google.maps.GeocoderStatus.OK){
+							alert("Stringa immessa non valida");
+						} else {
+							let coordPartenza = resultPartenza[0].geometry.location;
+							let coordArrivo = resultArrivo[0].geometry.location;
+							visualizzaPercorso(coordPartenza, coordArrivo);
+						};
+					});
+				};
+			});
+		};
+	});
+	
+	function visualizzaPercorso(partenza, arrivo){
+		let directionsService = new google.maps.DirectionsService();
+		let directionsRenderer;
+		
+		let percorso = {
+			"origin": partenza,
+			"destination": arrivo,
+			"travelMode": google.maps.TravelMode.DRIVING, // Default, in alternativa DRIVING, BICYCLING, TRANSIT o WALKING
+			"provideRouteAlternatives": true, // per default ne restituisce una 
+		};
+		
+		directionsService.route(percorso, function(routes, status){
+			if(status != google.maps.DirectionsStatus.OK) {
+				alert("percorso non valido");
+			} else {
+				let mapOptions = {
+					"center": partenza,
+					"zoom":16, 
+					"mapTypeId": google.maps.MapTypeId.HYBRID,
+				};
+				let mappa = new google.maps.Map(map, mapOptions);
+
+				for(let i = 0; i < routes.routes.length; i++){
+					let color = "rgb(127, 127, 127)";
+					if (i == 0) {
+						color = "blue";
+					};
+
+					let renderOptions = {
+						polylineOptions: {
+							strokeColor: color,
+							strokeWeight: 6,
+							zIndex: 100 - i, 
+						}
+					};
+
+					directionsRenderer = new google.maps.DirectionsRenderer(renderOptions);
+				    directionsRenderer.setMap(mappa);
+					directionsRenderer.setRouteIndex(i);
+					directionsRenderer.setDirections(routes);
+				}
+				
+				directionsRenderer.setPanel(panel);	
+
+				let distanza = routes.routes[0].legs[0].distance.text;
+				let durata = routes.routes[0].legs[0].duration.text;
+				msg.html("Distanza: " + distanza + "<br>" + "Durata: " + durata);
+			}
+		});
+	};
+});
